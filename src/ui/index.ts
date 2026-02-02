a85843cc392a431fe066e8e12f03f97ce780374f
Franz Evangelista <evangelistafranz@gmail.com>
2026-02-02 20:15:11 +0800

    feat: add OrderConfirmed component with styles and Storybook stories for order confirmation display

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,5 +2,6 @@ export * from "./AddToCart";
 export * from "./Button";
 export * from "./Cart";
 export * from "./CartItems";
+export * from "./OrderConfirmed";
 export * from "./ProductCard";
 export * from "./ProductGrid";
