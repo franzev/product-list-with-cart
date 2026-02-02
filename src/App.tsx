9c50ce911734c0d18ab7cb08ca41f97e7fd9b4f5
Franz Evangelista <evangelistafranz@gmail.com>
2026-02-02 18:50:34 +0800

    feat: implement App component with state management for cart items and responsive layout styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,62 @@
+import { useState } from "react";
+import { CartSection, ProductGrid } from "./ui";
+import styles from "./App.module.css";
+import productsData from "../data.json";
+import type { Product } from "./types";
+import type { CartItem } from "./types";
+
 function App() {
+  const [items, setItems] = useState<CartItem[]>(
+    productsData.map((product) => ({
+      product,
+      quantity: 0,
+    }))
+  );
+
+  const handleIncrement = (product: Product) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.product.name === product.name
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
+  };
+
+  const handleDecrement = (product: Product) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.product.name === product.name && item.quantity > 0
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+    );
+  };
+
+  const handleRemoveItem = (itemToRemove: CartItem) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.product.name === itemToRemove.product.name
+          ? { ...item, quantity: 0 }
+          : item
+      )
+    );
+  };
 
   return (
-    <>
+    <main className={styles.base}>
+      <section className={styles.products}>
+        <h1>Desserts</h1>
+        <ProductGrid
+          items={items}
+          onDecrement={handleDecrement}
+          onIncrement={handleIncrement}
+        />
+      </section>
 
-    </>
-  )
+      <CartSection items={items} onRemoveItem={handleRemoveItem} />
+    </main>
+  );
 }
 
-export default App
+export default App;
