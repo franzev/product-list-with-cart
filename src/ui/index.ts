7d1379e068332f2f7a0c9d05a18a89a42a98e2e4
Franz Evangelista <evangelistafranz@gmail.com>
2026-02-02 21:47:12 +0800

    feat: add Modal component with animations, styles, and Storybook stories for enhanced user interaction

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,6 +2,7 @@ export * from "./AddToCart";
 export * from "./Button";
 export * from "./Cart";
 export * from "./CartItems";
+export * from "./Modal";
 export * from "./OrderConfirmed";
 export * from "./ProductCard";
 export * from "./ProductGrid";
