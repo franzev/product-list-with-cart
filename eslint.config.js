0c3ae87b26686fa55311d5f42a709212ecc39aea
Franz Evangelista <evangelistafranz@gmail.com>
2026-02-03 17:13:32 +0800

    chore: standardize import statements and update ESLint and Vite configuration for improved consistency and readability

--- eslint.config.js
+++ eslint.config.js
@@ -5,25 +5,29 @@ import storybook from "eslint-plugin-storybook";
 // import globals from 'globals'
 // import reactHooks from 'eslint-plugin-react-hooks'
 // import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default defineConfig([globalIgnores(['dist']), {
-  files: ['**/*.{ts,tsx}'],
-  extends: [
-    // js.configs.recommended,
-    // tseslint.configs.recommended,
-    // reactHooks.configs.flat.recommended,
-    // reactRefresh.configs.vite,
-    tseslint.configs.recommendedTypeChecked,
-    tseslint.configs.strictTypeChecked,
-    tseslint.configs.stylisticTypeChecked,
-  ],
-  languageOptions: {
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      // js.configs.recommended,
+      // tseslint.configs.recommended,
+      // reactHooks.configs.flat.recommended,
+      // reactRefresh.configs.vite,
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+      // other options...
     },
-    // other options...
   },
-}, ...storybook.configs["flat/recommended"]])
+  ...storybook.configs["flat/recommended"],
+]);
